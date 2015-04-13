033c66b6b27a7a71e4f4dbf0f03e43ada1a11b31
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-13 20:43:26 +0200

    Add library.

---
+++ src/helpers.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Str;
+
+if (!function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     * Supports boolean, empty and null.
+     *
+     * @param string $key
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if (!$value) {
+            return value($default);
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        if (Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+}
+
+if (!function_exists('format_bytes')) {
+    /**
+     * Format bytes to kilobytes, megabytes, gigabytes.
+     *
+     * @param int $bytes
+     * @param  int $precision
+     * @return string
+     */
+    function format_bytes($bytes, $precision = 2)
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1024, $pow);
+
+        return sprintf('%s %s', round($bytes, $precision), $units[$pow]);
+    }
+}
---
+++ src/modules/theme.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of WordPlate.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ /**
+ * Fix home URL on theme activation
+ *
+ * @return void
+ */
+add_action('after_setup_theme', function () {
+    $url = get_option('home');
+    if (ends_with($url, '/wordpress')) {
+        update_option('home', str_replace('/wordpress', '', $url));
+    }
+});

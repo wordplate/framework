c589c79e8890c34f060d2e0777ac6b51b108bba9
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-19 18:40:57 +0200

    Add default pages
    This closes #4

---
+++ components/pages.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of WordPlate.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Create default pages specified in configuration.
+ *
+ * @return void
+ */
+add_action('admin_init', function () {
+    $pages = config('pages.pages');
+
+    if (!is_array($pages) || count($pages) <= 0) {
+        return;
+    }
+
+    // Create the default pages.
+    foreach ($pages as $page) {
+        if (!post_exists($page['title'])) {
+            wp_insert_post([
+                'post_title' => $page['title'],
+                'post_status' => 'publish',
+                'post_type' => 'page',
+            ]);
+        }
+    }
+
+    foreach ($pages as $page) {
+        $page_id = get_page_by_title($page['title'])->ID;
+
+        if (isset($page['template'])) {
+            // Set page template.
+            wp_update_post([
+                'ID' => $page_id,
+                'page_template' => $page['template'],
+            ]);
+        }
+
+        if (isset($page['type']) && str_is($page['type'], 'home')) {
+            update_option('show_on_front', 'page');
+            update_option('page_on_front', $page_id);
+
+            // Set home page.
+            wp_update_post([
+                'ID' => $page_id,
+                'menu_order' => -1
+            ]);
+        }
+
+        if (isset($page['type']) && str_is($page['type'], 'blog')) {
+            // Set blog page.
+            update_option('page_for_posts', $page_id);
+        }
+
+        if (isset($page['parent'])) {
+            $parent = get_page_by_title($page['parent']);
+
+            // Set parent page.
+            wp_update_post([
+                'ID' => $page_id,
+                'post_parent' => $parent->ID,
+            ]);
+        }
+    }
+});

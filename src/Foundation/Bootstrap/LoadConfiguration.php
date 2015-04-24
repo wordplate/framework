25f4b1faf803b1e4239bc6d0f79c5f6d3fc4cb20
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-24 16:18:06 +0200

    Fixes a bug introduced in #11

--- src/Foundation/Bootstrap/LoadConfiguration.php
+++ src/Foundation/Bootstrap/LoadConfiguration.php
@@ -37,7 +37,7 @@ class LoadConfiguration
 
         $this->loadConfigurationFiles($app, $config);
 
-        date_default_timezone_set($config['app.timezone']);
+        date_default_timezone_set($config['theme.timezone']);
 
         mb_internal_encoding('UTF-8');
     }

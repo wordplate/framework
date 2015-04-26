4cf54cfd2c853809a01621a0c33859d28d03cdbb
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-26 15:47:05 +0200

    Remove ACF simple editor toolbar
    This closes #14

--- components/editor.php
+++ components/editor.php
@@ -10,24 +10,6 @@
  */
 
 /**
- * Add a simpler editor toolbar to ACF.
- *
- * @param array $toolbars
- *
- * @return void
- */
-add_filter('acf/fields/wysiwyg/toolbars', function ($toolbars) {
-    $toolbars['Simple'] = [];
-    $toolbars['Simple'][1] = ['bold', 'italic', 'underline'];
-
-    if (!array_search('code', $toolbars['Full'][2])) {
-        unset($toolbars['Full'][2][$key]);
-    }
-
-    return $toolbars;
-});
-
-/**
  * Force perfect JPG images.
  *
  * @return int

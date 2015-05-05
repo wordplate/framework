34d152015b4e296132510d767f843d6dbad50cd5
Vincent Klaiber <vincentklaiber@gmail.com>
2015-05-05 15:59:37 +0200

    Use Whoops for all errors

--- src/Debug/ExceptionHandler.php
+++ src/Debug/ExceptionHandler.php
@@ -41,83 +41,9 @@ class ExceptionHandler
     {
         $this->app = $app;
 
-        error_reporting(-1);
-
-        set_error_handler([$this, 'handleError']);
-
-        set_exception_handler([$this, 'handleException']);
-
-        register_shutdown_function([$this, 'handleShutdown']);
-    }
-
-    /**
-     * Handle an uncaught exception from the application.
-     *
-     * @param \Exception $e
-     *
-     * @return void
-     */
-    public function handleException(Exception $e)
-    {
         if (config('theme.debug')) {
-            return $this->whoops()->handleException($e);
+            $this->whoops()->register();
         }
-    }
-
-    /**
-     * Convert a PHP error to an ErrorException.
-     *
-     * @param int $level
-     * @param string $message
-     * @param string $file
-     * @param int $line
-     *
-     * @throws \ErrorException
-     */
-    public function handleError($level, $message, $file = '', $line = 0)
-    {
-        if (error_reporting() & $level) {
-            throw new ErrorException($message, 0, $level, $file, $line);
-        }
-    }
-
-    /**
-     * Handle the PHP shutdown event.
-     *
-     * @return void
-     */
-    public function handleShutdown()
-    {
-        if (!is_null($error = error_get_last()) && $this->isFatal($error['type'])) {
-            $this->handleException($this->fatalExceptionFromError($error, 0));
-        }
-    }
-
-    /**
-     * Create a new fatal exception instance from an error array.
-     *
-     * @param array $error
-     * @param int|null $traceOffset
-     *
-     * @return \Symfony\Component\Debug\Exception\FatalErrorException
-     */
-    protected function fatalExceptionFromError(array $error, $traceOffset = null)
-    {
-        return new FatalErrorException(
-            $error['message'], $error['type'], 0, $error['file'], $error['line'], $traceOffset
-        );
-    }
-
-    /**
-     * Determine if the error type is fatal.
-     *
-     * @param int $type
-     *
-     * @return bool
-     */
-    protected function isFatal($type)
-    {
-        return in_array($type, [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE]);
     }
 
     /**

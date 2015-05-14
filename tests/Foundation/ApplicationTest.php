6d0e5ce2785eb9977f5f218a3e4997b72c7dee99
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-05-14 20:38:10 +0200

    Added test suite
    Fixed coding style
    
    Added php 7.0 and hhvm
    
    Added php 5.4.7
    
    Specified version number for wp-test-suite
    
    Fixed classmap. Updated test comment
    
    Updated composer with classmap
    
    Changed to .. instead of dirname
    
    Updated phpunit.xml.dist

---
+++ tests/Foundation/ApplicationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of WordPlate.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WordPlate\tests\Foundation;
+
+use WordPlate\Foundation\Application;
+
+/**
+ * This is the application test class.
+ *
+ * @author Fredrik Forsmo <fredrik.forsmo@gmail.com>
+ */
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test application class.
+     *
+     * @return void
+     */
+    public function testApplicationClass()
+    {
+        try {
+            new Application;
+        } catch (\Exception $e) {
+            $this->assertEquals('The "/config" directory does not exist.', $e->getMessage());
+        }
+    }
+}

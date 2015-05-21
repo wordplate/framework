f78bd437e338d1302174e3ebbabbad8f10de95bb
Vincent Klaiber <vincentklaiber@gmail.com>
2015-05-21 12:56:48 +0200

    Update version number to 2.5.0

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -27,7 +27,7 @@ class Application extends SymfonyApplication
      *
      * @var string
      */
-    protected $version = '2.4.0';
+    protected $version = '2.5.0';
 
     /**
      * The applications base path.

ef6ca976ef7076664c2f7f86ea36e0a813eaffa6
Vincent Klaiber <vincentklaiber@gmail.com>
2015-06-02 12:14:03 +0200

    Bump version number

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -27,7 +27,7 @@ class Application extends SymfonyApplication
      *
      * @var string
      */
-    protected $version = '2.5.2';
+    protected $version = '2.5.3';
 
     /**
      * The applications base path.

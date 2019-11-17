cf59ae78db015e7544588ddb484d65089bc9973f
Lukas K <34108423+lkaisers@users.noreply.github.com>
2019-11-17 16:57:36 +0100

    Register WP_CACHE constant (#93)
    * Register WP_CACHE constant
    
    * Update Application.php
    
    
    Co-authored-by: Vincent Klaiber <vinkla@users.noreply.github.com>

--- src/Application.php
+++ src/Application.php
@@ -122,6 +122,9 @@ class Application
         // https://make.wordpress.org/core/2019/04/16/fatal-error-recovery-mode-in-5-2/
         define('WP_DISABLE_FATAL_ERROR_HANDLER', env('WP_DISABLE_FATAL_ERROR_HANDLER', false));
 
+        // Set the cache constant for plugins such as WP Super Cache and W3 Total Cache.
+        define('WP_CACHE', env('WP_CACHE', true));
+
         // Set the absolute path to the WordPress directory.
         if (!defined('ABSPATH')) {
             define('ABSPATH', sprintf('%s/%s/', $this->getPublicPath(), env('WP_DIR', 'wordpress')));

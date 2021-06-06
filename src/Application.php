c9b468a9143c67b70fc30fc67698820ca13afe31
Vincent Klaiber <vincent@doubledip.se>
2021-06-06 15:48:48 +0200

    WP_THEME → WP_DEFAULT_THEME

--- src/Application.php
+++ src/Application.php
@@ -86,7 +86,7 @@ class Application
         define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS', 7));
 
         // Set the default WordPress theme.
-        define('WP_DEFAULT_THEME', env('WP_THEME', 'wordplate'));
+        define('WP_DEFAULT_THEME', env('WP_DEFAULT_THEME', env('WP_THEME', 'wordplate'))); // TODO: Remove WP_THEME in the next major version
 
         // Constant to configure core updates.
         define('WP_AUTO_UPDATE_CORE', env('WP_AUTO_UPDATE_CORE', 'minor'));
